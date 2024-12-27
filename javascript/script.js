a7091dee4b476343e08237edf4a35a7a7db85216
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-27 10:43:47 +0100

    introduction de la librairie scrollReveal

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,11 @@
 const title=document.querySelector("h1")
+const app=new Typewriter(title, {
+    delay:200,
+    loop:true
+
+})
+.typeString("Le paradis sucré votre destination <span>Gourmande</span>")
+.pauseFor(3000)
+.deleteAll()
+.pauseFor(2000)
+.start();
